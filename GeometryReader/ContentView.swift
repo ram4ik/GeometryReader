6ac41a1976ed0b4a0ed2e3572077a45499941927
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-14 18:42:21 +0200

    init

--- GeometryReader/ContentView.swift
+++ GeometryReader/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader_Intro()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GeometryReader_Intro: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("GeometryReader")
+                .font(.largeTitle)
+            
+            Text("Introduction")
+                .font(.title)
+                .foregroundColor(.gray)
+            
+            Text("GeometryReader is a container view that pushes out to fill up all available space. You use it to help with positioning items within it.")
+                .font(.title)
+                .padding()
+                .layoutPriority(1)
+            
+            GeometryReader { _ in
+                Text("Views center automatically inside the GeometryReader")
+                    .font(.title)
+                
+            }
+            .foregroundColor(Color.white)
+            .background(Color.pink)
+        }
     }
 }
